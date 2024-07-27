74866eab41f7043b39601998badf8f5bbd9e22d3
basit1215 <haizabdulbasit225@gmail.com>
2024-07-28 04:35:37 +0500

    updated

---
+++ Chap-02/chapTwo.js
@@ -0,0 +1,8 @@
+var name = "Basit";
+name ="Abdul Basit";
+console.log(name);
+
+var country ;
+country = "Pakistan";
+console.log(country);
+
---
+++ Chap-03/chapThree.js
@@ -0,0 +1,19 @@
+var weight= 40;
+weight + 20;
+console.log(weight);
+
+var orgNum = 25;
+var updtNum = orgNum + 10;
+console.log(updtNum);
+
+var numb = 25;
+var numbToAdd = 10;
+console.log(numb + numbToAdd);
+
+var num = 25;
+num = num + 10;
+console.log(num);
+
+var originalNum = "25";
+var newNum = originalNum + 10;
+console.log(newNum)
