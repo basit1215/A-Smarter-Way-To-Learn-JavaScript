8291f6bf57cfed2f4ab7e0a1607d761ca41fa372
basit1215 <haizabdulbasit225@gmail.com>
2024-07-28 14:39:32 +0500

    updated

---
+++ Chap-05/chapFive.js
@@ -0,0 +1,20 @@
+var addNum = 2 + 2;
+console.log(addNum);
+
+var sbtrNum = 12 - 6;
+console.log(sbtrNum);
+
+var num =20;
+var newNum = num + 40;
+console.log(newNum);
+
+var newNumb = 5;
+newNumb = newNumb + 10;
+console.log(newNumb);
+
+var reminder1 = 7 % 3;
+console.log(reminder1);
+
+var reminder2 = 9 % 3;
+console.log(reminder2);
+
---
+++ Chap-06/chapSix.js
@@ -0,0 +1,34 @@
+
+//  <<============ POST INCREAMENT ============>> 
+
+var num1 = 5;
+console.log(num1++);
+console.log(num1);
+
+var numb = 20;
+console.log(numb);
+numb++;
+console.log(numb);
+
+//  <<============ POST DECREAMENT ============>> 
+
+var number1 = 15;
+console.log(number1--);
+console.log(number1);
+
+var number2 = 10;
+console.log(number2);
+number2--;
+console.log(number2);
+
+//  <<============ PRE INCREAMENT ============>> 
+
+var num = 25;
+console.log(++num);
+console.log(num);
+
+//  <<============ PRE DECREAMENT ============>> 
+
+var num2 = 30;
+console.log(--num2);
+console.log(num2);
