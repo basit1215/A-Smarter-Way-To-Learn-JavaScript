0c35957d20e3ace3431014fe6cca366070f19fdb
basit1215 <haizabdulbasit225@gmail.com>
2024-07-28 14:54:38 +0500

    updated

---
+++ Chap-07/chapSeven.js
@@ -0,0 +1,18 @@
+var num = 1  + 2 * 7;
+console.log(num);
+
+var num2 = (1 + 2) * 7;
+console.log(num2);
+
+var num3 = 1 + (2 * 7);
+console.log(num3);
+
+var num4 = (2 + 8) * ( 9 + 5);
+console.log(num4);
+
+var num5 = 2 + 8 * 9 + 5;
+console.log(num5);
+
+var num6 = ((2 + 8) * 9) + 5;
+console.log(num6);
+
