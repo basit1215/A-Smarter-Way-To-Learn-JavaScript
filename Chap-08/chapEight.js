d3012f57c6b3e9d917f2396aa461b99749bdc6a1
basit1215 <haizabdulbasit225@gmail.com>
2024-07-28 15:08:40 +0500

    updated

---
+++ Chap-08/chapEight.js
@@ -0,0 +1,10 @@
+var firstName= "Abdul";
+var lastName = "Basit";
+var fullName = firstName + " " + lastName;
+alert( "My name is:" + " " + fullName + "!");
+
+var str = "Thanks";
+var userName = "Basit"
+var char = "!";
+var cmpltMsg = str + " " + userName + " " + char 
+alert(cmpltMsg);
