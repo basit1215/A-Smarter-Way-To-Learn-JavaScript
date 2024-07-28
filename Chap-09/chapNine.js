3ddf534b88647521f88532624fed138018809bc3
basit1215 <haizabdulbasit225@gmail.com>
2024-07-28 15:53:21 +0500

    updated

---
+++ Chap-09/chapNine.js
@@ -0,0 +1,9 @@
+var firstName= prompt("Enter your First Name:");
+var lastName= prompt("Enter your Last Name:");
+var userMsg= alert("Assalam oa alikum " + firstName + " "  + lastName + "!");
+
+var qualification = prompt("Enter your Last Qualification:" , "Intermeddiate");
+console.log(qualification);
+
+var number = +prompt("Enter your Phone Number:");
+console.log(number);
